0e4f17e4a7d8c8b01188b75c86b6053ba35bf74f
itamakatz <itamakatz@gmail.com>
2021-04-28 02:27:11 -0700

    finished the equivalent vector

--- Exercises/Ex2/ex2.m
+++ Exercises/Ex2/ex2.m
@@ -1,10 +1,12 @@
 % a = [ 25 0 0 -13 11 0 7 0 0 0 0 ]
-a = [ 5 0 1 0 0 ]
-
+a = [ 5 0 1 0 0 3 0 0 0 ]
 k1 = 2
 k2 = 2
 k3 = 2
-encode_equivalent_vector(a, k1,k2,k3)
+N = 3
+encoded = encode_equivalent_vector(a, k1,k2,k3)
+vec = decode_equivalent_vector(encoded, N,k1,k2,k3)
+assert(isequal(vec,a))
 % benchmark()
 % quantization(randi([-100, 100],1,10)./200, 0.02)
 % golomb_rice_test()
@@ -151,68 +153,10 @@ function benchmark()
 end
 
 % =========== E: Benchmark =========== %
-% =========== B: 4 =========== %
 
-% =========== E: 4 =========== %
+% =========== B: 6 =========== %
 
-% =========== B: Equivalent Vector =========== %
-
-function encoded = encode_equivalent_vector(vec,k1,k2,k3)
-	integers = [];
-	runs = [];
-	last_zero = find(vec,1,'last');
-	fist_time = true;
-	for i = 1:last_zero
-		if(vec(i) == 0 && ~fist_time)
-			runs(end) = runs(end) + 1;
-		else
-			fist_time = false;
-			runs = [runs 0];
-			integers = [integers vec(i)];
-		end
-	end
-
-	assert(runs(end) == 0);
-	runs = runs(1:end-1);
-	assert(runs(end) ~= 0);
-
-	last_zero_encoded = golomb_rice(last_zero,k1)
-
-	runs_encoded_cells = arrayfun(@(x) golomb_rice(x,k3),runs, 'UniformOutput', false);
-	runs_encoded_cells = reshape(runs_encoded_cells, 1, numel(runs_encoded_cells));
-	runs_encoded = cell2mat(runs_encoded_cells);
-	runs_encoded = cast(runs_encoded,'char')
-
-	integers_unsigned =  arrayfun(@(x) ToUnsigned(x), integers(2:end));
-	integers_encoded_cells = arrayfun(@(x) exp_golomb(x,k3),integers_unsigned, 'UniformOutput', false);
-	integers_encoded_cells = reshape(integers_encoded_cells, 1, numel(integers_encoded_cells));
-	integers_encoded = cell2mat(integers_encoded_cells)
-
-	first_integer_bits = dec2bin(integers(1))
-
-	encoded = [last_zero_encoded runs_encoded first_integer_bits integers_encoded];
-end
-
-function vec = deencode_equivalent_vector(encoded,k1,k2,k3)
-
-	index = 1;
-	last_zero = golomb_rice_inverse(encoded,k1);
-	last_zero_length = golomb_rice_length(last_zero,k1);
-	index = index + last_zero_length;
-	
-	runs_encoded_cells = arrayfun(@(x) golomb_rice(x,k2),runs, 'UniformOutput', false);
-	runs_encoded_cells = reshape(runs_encoded_cells, 1, numel(runs_encoded_cells));
-	runs_encoded = cell2mat(runs_encoded_cells);
-
-	integers_unsigned =  arrayfun(@(x) ToUnsigned(x), integers);
-	integers_encoded_cells = arrayfun(@(x) exp_golomb(x,k3),integers_unsigned, 'UniformOutput', false);
-	integers_encoded_cells = reshape(integers_encoded_cells, 1, numel(integers_encoded_cells));
-	integers_encoded = cell2mat(integers_encoded_cells);
-	
-	encoded = [last_zero_encoded runs_encoded integers_encoded];
-end
-
-% =========== E: Equivalent Vector =========== %
+% =========== E: 6 =========== %
 
 % =========== B: Golomb-Rice =========== %
 
@@ -225,27 +169,37 @@ function codeBits = golomb_rice(num, k)
 	if(len > k)
 		suffix = numCharArray(end-k+1:end);
 		remainder = bin2dec(numCharArray(1:end-k));
-		prefix = zeros([1,remainder])+'0';
-		codeBits = [prefix 1+'0' suffix];
+		prefix = ones([1,remainder])+'0';
+		codeBits = [prefix 0+'0' suffix];
 	else
 		suffix = zeros([1,k])+'0';
 		suffix(end-len+1:end) = numCharArray;
-		codeBits = [1+'0' suffix];
+		codeBits = [0+'0' suffix];
 	end
 end
 
 % Tested
 function num = golomb_rice_inverse(codeBits, k)
 	i = 0;
-	while (codeBits(i+1) == '0')
+	while (codeBits(i+1) == '1')
 		i = i +1;
 	end
 	binary_val = [dec2bin(i) codeBits(end-k+1:end)];
 	num = bin2dec(binary_val);
 end
 
+% Tested
 function len = golomb_rice_length(num, k)
 	len = length(golomb_rice(num, k));
+end
+
+% Tested
+function subCodeBits = golomb_rice_find_prefix(codeBits, k)
+	i = 1;
+	while (codeBits(i) == '1')
+		i = i +1;	
+	end
+	subCodeBits = codeBits(1:k+i);
 end
 
 % Tested
@@ -259,6 +213,18 @@ function golomb_rice_test()
 		num = golomb_rice_inverse(codeBits, k)
 		assert(isequal(a,num))
 	end
+
+	for index = 1:100
+		clc
+		a = randi([0,1000])
+		k = randi([0,7])
+		codeBits = golomb_rice(a,k)
+		suffix = randi([0,1],1,5) + '0';
+		extendedCodeBits = [codeBits suffix]
+		subCodeBits = golomb_rice_find_prefix(extendedCodeBits,k)
+		num = golomb_rice_inverse(subCodeBits, k)
+		assert(isequal(a,num))
+	end
 end
 
 % =========== E: Golomb-Rice =========== %
@@ -266,54 +232,57 @@ end
 
 % Tested
 function codeBits = exp_golomb(num, k)
-	% accepts negative numbers and simply adds a sign bit at the end. 0 is positive, 1 negative
-	if(num > 0)
-		sign_bit = 0 +'0';
-	else
-		sign_bit = 1 +'0';
-	end
-	num = abs(num);
-	% assert(num >= 0);
+	assert(num >= 0);
 	num = num+2.^k-1;
 	numCharArray = dec2bin(num+1);
 	len = length(numCharArray)-1;
 	uncutCode = [(zeros(1,len)+'0') numCharArray];
 	codeBits = uncutCode(k+1:end);
-	codeBits = [codeBits sign_bit];
 end
 
 % Tested
 function num = exp_golomb_inverse(codeBits, k)
-	% can return a negative number
-	codeBitsFull = [zeros(1,k)+'0', codeBits(1:end-1)];
+	codeBitsFull = [zeros(1,k)+'0', codeBits(1:end)];
 	cutIndex = find(codeBitsFull-'0', 1, 'first');
 	codeBitsCut = codeBitsFull(cutIndex:end);
 	num = bi2de(codeBitsCut-'0','left-msb')-1;
 	num = num-2.^k+1 ;
-	if(codeBits(end) == '1')
-		num = num * -1;
-	end
 end
 
 % Tested
 function lengthArray = exp_golomb_lengths(array, k)
-	% length for the code + 1 sign bit
-	array = abs(array) % calculate length for the abs values and add one at the end
 	ys = arrayfun(@(x)floor(x/2.^k), array);
-	lengthArray = arrayfun(@(x)2*floor(log2(double(x+1)))+1, ys) + k + 1; % the last + 1 is for the sign bit
+	lengthArray = arrayfun(@(x)2*floor(log2(double(x+1)))+1, ys) + k;
+end
+
+function subCodeBits = exp_golomb_find_prefix(codeBits, k)
+	i = 0;
+	while (codeBits(i+1) == '0')
+		i = i + 1;
+	end
+	subCodeBits = codeBits(1:k+i+i+1);
 end
 
 function exp_golomb_test()
 	for index = 1:100
 		clc
-		a = randi([-1000,1000])
+		a = randi([0,1000])
 		k = randi([0,7])
 		codeBits = exp_golomb(a,k)
-		negative_codeBits = exp_golomb(a*-1,k)
-		assert(isequal(codeBits(1:end-1), negative_codeBits(1:end-1)));
-		assert(~isequal(codeBits(end), negative_codeBits(end)));
 		assert(exp_golomb_lengths(a,k) == length(codeBits));
 		c = exp_golomb_inverse(codeBits,k)
+		assert(isequal(a,c));
+	end
+
+	for index = 1:100
+		clc
+		a = randi([0,1000])
+		k = randi([0,7])
+		codeBits = exp_golomb(a,k)
+		suffix = randi([0,1],1,5) + '0';
+		extendedCodeBits = [codeBits suffix]
+		subCodeBits = exp_golomb_find_prefix(extendedCodeBits,k)
+		c = exp_golomb_inverse(subCodeBits,k)
 		assert(isequal(a,c));
 	end
 end
@@ -354,3 +323,80 @@ function val = ToSigned(x)
 end
 
 % =========== E: Signed / Unsigned =========== %
+
+% =========== B: Equivalent Vector =========== %
+
+function encoded = encode_equivalent_vector(vec,k1,k2,k3)
+	integers = [];
+	runs = [];
+	last_non_zero = find(vec,1,'last');
+	fist_time = true;
+	for i = 1:last_non_zero
+		if(vec(i) == 0 && ~fist_time)
+			runs(end) = runs(end) + 1;
+		else
+			fist_time = false;
+			runs = [runs 0];
+			integers = [integers vec(i)];
+		end
+	end
+
+	assert(runs(end) == 0);
+	runs = runs(1:end-1);
+	assert(runs(end) ~= 0);
+
+	last_non_zero_encoded = golomb_rice(last_non_zero,k1);
+
+	runs_encoded_cells = arrayfun(@(x) golomb_rice(x,k3),runs, 'UniformOutput', false);
+	runs_encoded_cells = reshape(runs_encoded_cells, 1, numel(runs_encoded_cells));
+	runs_encoded = cell2mat(runs_encoded_cells);
+	runs_encoded = cast(runs_encoded,'char');
+
+	integers_unsigned = arrayfun(@(x) ToUnsigned(x), integers(2:end));
+	integers_encoded_cells = arrayfun(@(x) exp_golomb(x,k3), integers_unsigned, 'UniformOutput', false);
+	integers_encoded_cells = reshape(integers_encoded_cells, 1, numel(integers_encoded_cells));
+	integers_encoded = cell2mat(integers_encoded_cells)
+
+	first_integer_bits = dec2bin(integers(1));
+	first_integer_bits = [zeros([1, 8-length(first_integer_bits)])+'0' first_integer_bits];
+	encoded = [last_non_zero_encoded runs_encoded first_integer_bits integers_encoded];
+end
+
+function vec = decode_equivalent_vector(encoded,N,k1,k2,k3)
+	sub_encoded = golomb_rice_find_prefix(encoded,k1);
+	last_non_zero = golomb_rice_inverse(sub_encoded,k1);
+	last_non_zero_length = golomb_rice_length(last_non_zero,k1);
+	encoded = encoded(last_non_zero_length+1:end);
+
+	runs = [];
+	num_count = 1;
+	while(num_count < last_non_zero)
+		sub_encoded = golomb_rice_find_prefix(encoded,k2);
+		val = golomb_rice_inverse(sub_encoded,k2);
+		val_length = golomb_rice_length(val,k2);
+		encoded = encoded(val_length+1:end);
+		num_count = num_count + val + 1;
+		runs = [runs val];
+	end
+
+	first_integer = bin2dec(encoded(1:8));
+	encoded = encoded(8+1:end);
+	integers = zeros([1,length(runs)]);
+	for i = 1:length(runs)
+		sub_encoded = exp_golomb_find_prefix(encoded,k3);
+		val = exp_golomb_inverse(sub_encoded,k3);
+		val_length = exp_golomb_lengths(val,k3);
+		encoded = encoded(val_length+1:end);
+		integers(i) = val;
+	end
+	
+	vec = zeros([1, N*N]);
+	vec(1) = first_integer;
+	vec_index = 1;
+	for i = 1:length(runs)
+		vec_index = vec_index + runs(i) + 1;
+		vec(vec_index) = ToSigned(integers(i));
+	end
+end
+
+% =========== E: Equivalent Vector =========== %
